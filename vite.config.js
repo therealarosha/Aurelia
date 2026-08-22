ad9637aa481087a031415244501c7b67169b5960
Arosha Dissanayaka <111232905+therealarosha@users.noreply.github.com>
2026-08-22 18:27:02 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
-  return {
-    plugins: [react()],
-    base: env.VITE_BASE_PATH || '/',
-    build: { sourcemap: false },
-  };
+export default defineConfig({
+  plugins: [react()],
+
+  // GitHub Pages repository path
+  base: '/Aurelia/',
+
+  build: {
+    sourcemap: false,
+  },
 });
